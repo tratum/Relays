26cb17ff54e20bd683963b566a544f668bb9ae5f
tratum <tratum.connect@gmail.com>
2026-08-06 17:42:34 +0530

    feat(subscriptions): define plan_features db table schema

---
+++ app/infra/db/schemas/01-tables/subscriptions/plan_features.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS plan_features (
+  plan_id SMALLINT NOT NULL,
+  feature_id SMALLINT NOT NULL
+);
---
+++ app/infra/db/schemas/02-constraints/subscriptions/plan_features.sql
@@ -0,0 +1,13 @@
+ALTER TABLE plan_features
+  ADD CONSTRAINT pk_plan_features
+    PRIMARY KEY (plan_id, feature_id),
+
+  ADD CONSTRAINT fk_plan_features_plan
+    FOREIGN KEY (plan_id)
+    REFERENCES plans(id)
+    ON DELETE RESTRICT,
+
+  ADD CONSTRAINT fk_plan_features_feature
+    FOREIGN KEY (feature_id)
+    REFERENCES features(id)
+    ON DELETE RESTRICT;
